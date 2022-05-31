691b0f19259a7c6da6c9cb99b74d2f0a23acd364
Kris Mally <kristophermally@gmail.com>
2022-05-31 15:07:50 -0600

    Add log Schema

--- controllers/server.js
+++ controllers/server.js
@@ -1,7 +1,9 @@
+// dependencies
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
+const Log = require("../models/logs.js");
 
 // database connection
 mongoose.connect(process.env.DATABASE_URL, {
@@ -16,6 +18,23 @@ db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
+// middleware
+
+// I
+
+// N
+
+// D
+
+// U
+
+// C
+
+// E
+
+// S
+
+// listener
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`express is listening on port ${PORT}`);
---
+++ models/logs.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const logSchema = new mongoose.Schema({
+    date: String,
+    dayRating: Number,
+    painLvl: Number,
+    fatigueLvl: Number,
+    cupsWater: Number,
+    sleepHrs: Number,
+    outsideTime: Number,
+    meds: String,
+    mood: String,
+    stretch: Boolean,
+    foodLog: Object,
+    dayNotes: String,
+});
+
+const Log = mongoose.model("Log", logSchema);
+module.exports = Log;
